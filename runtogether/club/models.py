79adea27cf980cf1eba1ea00865a380ef603fc27
Nicolas M <mine.nicolas@gmail.com>
2026-01-23 22:07:15 +0100

    play a bit with clubs

--- runtogether/club/models.py
+++ runtogether/club/models.py
@@ -4,7 +4,46 @@ from django_editorjs_fields import EditorJsJSONField
 
 class Club(models.Model):
     name = models.CharField(max_length=500, null=False, blank=False)
-    description = EditorJsJSONField()
+    description = EditorJsJSONField(
+        # https://github.com/editor-js/awesome-editorjs?tab=readme-ov-file
+        # plugins=[
+        #     "@editorjs/image",
+        #     "header-with-alignment",
+        #     "paragraph-with-alignment",
+        #     "@cychann/editorjs-quote",
+        #     "@editorjs/warning",
+        #     "@coolbytes/editorjs-delimiter",
+        #     "editorjs-alert",
+        #     "editorjs-color-picker",
+        #     "@skchawala/editorjs-text-style",
+        #     "@editorjs/list@latest",
+        #     "@editorjs/nested-list",
+        #     "@editorjs/image",
+        #     "@editorjs/simple-image",
+        #     "@editorjs/link@editorjs/table",
+        #     "editorjs-hyperlink",
+        # ],
+        # tools={
+        #     "Image": {
+        #         "config": {
+        #             "endpoints": {
+        #                 "byFile": "/editorjs/image_upload/"  # Your custom backend file uploader endpoint
+        #             }
+        #         }
+        #     },
+        # },
+        # i18n={
+        #     "messages": {
+        #         "blockTunes": {
+        #             "delete": {"Delete": "Удалить"},
+        #             "moveUp": {"Move up": "Переместить вверх"},
+        #             "moveDown": {"Move down": "Переместить вниз"},
+        #         }
+        #     },
+        # },
+        null=True,
+        blank=True,
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=True)
